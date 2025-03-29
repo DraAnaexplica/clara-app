aa0eeeb8a08f0e423bd53d9146bb13aeba0b81bd
Draanaexplica <seu-email-do-github@gmail.com>
2025-03-29 14:49:25 -0300

    ðŸŸ¢ AtualizaÃ§Ã£o automÃ¡tica

--- static/js/chat.js
+++ static/js/chat.js
@@ -173,3 +173,23 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+const avatar = document.getElementById("avatar");
+const modal = document.getElementById("image-modal");
+const modalImg = document.getElementById("modal-image");
+const close = document.getElementsByClassName("close")[0];
+
+avatar.onclick = function () {
+  modal.style.display = "block";
+  modalImg.src = avatar.src;
+};
+
+close.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
