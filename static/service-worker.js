9991aef245d61fdb48aa45c67a047de4e617abec
Draanaexplica <seu-email-do-github@gmail.com>
2025-03-31 17:40:54 -0300

    ðŸŸ¢ AtualizaÃ§Ã£o automÃ¡tica

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,13 +1,15 @@
+// Service Worker da Clara
+
 self.addEventListener("install", function (event) {
-    console.log("Service worker instalado.");
-    self.skipWaiting();
+    console.log("✅ Service Worker instalado.");
+    self.skipWaiting(); // Força ativação imediata
   });
   
   self.addEventListener("activate", function (event) {
-    console.log("Service worker ativado.");
+    console.log("✅ Service Worker ativado.");
   });
   
   self.addEventListener("fetch", function (event) {
-    // Apenas deixa passar as requisições
+    // Aqui deixamos todas as requisições passarem normalmente
   });
   
