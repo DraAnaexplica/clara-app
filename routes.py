f45c77aa2a9b16fca6007cee88cce464a7a3ff12
Draanaexplica <seu-email-do-github@gmail.com>
2025-04-02 12:58:43 -0300

    ðŸŸ¢ AtualizaÃ§Ã£o automÃ¡tica

--- routes.py
+++ routes.py
@@ -1,7 +1,35 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, redirect, url_for, make_response
+import datetime
 from openrouter_utils import gerar_resposta_clara
 
 app = Flask(__name__)
+
+# Tokens válidos
+TOKENS_VALIDOS = {
+    "teste123": {"expira": "2025-04-30"},
+    "vip456": {"expira": "2025-05-01"},
+}
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        token = request.form.get("token", "")
+        if token in TOKENS_VALIDOS:
+            resp = make_response(redirect(url_for('index')))
+            expira_em = datetime.datetime.now() + datetime.timedelta(days=30)
+            resp.set_cookie("token_clara", token, expires=expira_em)
+            return resp
+        else:
+            return render_template("login.html", erro="Token inválido.")
+    return render_template("login.html")
+
+@app.route('/')
+def index():
+    token = request.cookies.get("token_clara")
+    if not token or token not in TOKENS_VALIDOS:
+        return redirect(url_for("login"))
+    return render_template("index.html")
+
 
 @app.route('/clara', methods=['POST'])
 def conversar_com_clara():
@@ -13,10 +41,6 @@ def conversar_com_clara():
 
     resposta = gerar_resposta_clara(mensagem)
     return jsonify({'resposta': resposta})
-
-@app.route('/')
-def index():
-    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
