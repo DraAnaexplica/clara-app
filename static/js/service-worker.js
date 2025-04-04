24478a50001574b347f320d2bad591dfdea87d1d
Draanaexplica <seu-email-do-github@gmail.com>
2025-04-04 16:53:07 -0300

    ðŸŸ¢ AtualizaÃ§Ã£o automÃ¡tica

--- static/js/service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener("install", (event) => {
-  console.log("🛠️ Service Worker instalado");
-  self.skipWaiting();
-});
-self.addEventListener("activate", (event) => {
-  console.log("🚀 Service Worker ativado");
-});
-self.addEventListener("fetch", (event) => {});
